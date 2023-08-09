4b436ac0f59e1705e8690fadf612c4d2e9d23628
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-09 20:23:21 +0200

    feat: functions ts

---
+++ bases/funciones/functions.ts
@@ -0,0 +1,15 @@
+(() => {
+  const hero: string = "Flash";
+
+  function returnName(): string {
+    return hero;
+  }
+
+  const activateBatSignal = (): string => {
+    return "Batiseñal activada!";
+  };
+
+  console.log(typeof activateBatSignal);
+
+  const heroName: string = returnName();
+})();
