1da18439c64608297224fa0c8920ab9a9a3506ed
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-10 08:43:29 +0200

    feat: objects in ts intro

---
+++ bases/objects/objects.js
@@ -0,0 +1,19 @@
+"use strict";
+(() => {
+    // type flash : {
+    //   name: string;
+    //   age: number;
+    //   powers: string[];
+    //   name2?: string;
+    // };
+    let flash = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Super velocidad", "Viajar en el tiempo"],
+    };
+    flash = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["Super fuerza"],
+    };
+})();
---
+++ bases/objects/objects.ts
@@ -0,0 +1,20 @@
+(() => {
+  // type flash : {
+  //   name: string;
+  //   age: number;
+  //   powers: string[];
+  //   name2?: string;
+  // };
+
+  let flash = {
+    name: "Barry Allen",
+    age: 24,
+    powers: ["Super velocidad", "Viajar en el tiempo"],
+  };
+
+  flash = {
+    name: "Clark Kent",
+    age: 60,
+    powers: ["Super fuerza"],
+  };
+})();
