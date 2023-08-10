e31923dfbc135d72cb576c8a1f69509d55544297
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-10 18:37:42 +0200

    feat: ts classes

--- bases/main.js
+++ bases/main.js
@@ -1,52 +1,15 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: "Samuel L. Jackson",
-        ironman: "Robert Downey Jr.",
-        vision: "Paul Bettany",
-        activo: true,
-        poder: 1500,
-    };
-    const { poder: power, vision } = avengers;
-    console.log(power, vision.toUpperCase());
-    const printAvenger = (_a) => {
-        var { vision } = _a, resto = __rest(_a, ["vision"]);
-        return console.log(vision, resto);
-    };
-    printAvenger(avengers);
-    const avengersArr = ["Cap. América", "Ironman", "Hulk"];
-    const [capi, ironman] = avengersArr;
-    console.log({ ironman, capi });
-})();
-(() => {
-    const ironman = {
-        name: "Ironman",
-        weapon: "Armorsuit",
-    };
-    const capi = {
-        name: "Captain America",
-        weapon: "Shield",
-    };
-    const thor = {
-        name: "Thorcuato",
-        weapon: "Mjölnir",
-    };
-    const avengers = [ironman, capi, thor];
-    for (const avenger of avengers) {
-        console.log(avenger);
     }
-})();
-(() => {
+    Avenger.avgAge = 35;
+    const antman = new Avenger("Antman", "Capitan");
+    console.log(antman);
+    console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
