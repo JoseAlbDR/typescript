7f871de1ed45a7b4336645780398746b22492c67
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-10 23:06:14 +0200

    feat: namespaces

--- bases/main.js
+++ bases/main.js
@@ -1,41 +1,8 @@
 "use strict";
-const conducirBatimovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelerar();
-};
-const batimovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelerar() {
-        console.log("...... gogogo!!!");
-    },
-};
-conducirBatimovil(batimovil);
-const guason = {
-    reir: true,
-    comer: true,
-    llorar: false,
-};
-const reir = (guason) => {
-    if (guason.reir) {
-        console.log("JAJAJAJA");
-    }
-};
-const ciudadGotica = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    constructor(nombre, edad, sexo, estadoCivil) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sexo = sexo;
-        this.estadoCivil = estadoCivil;
-    }
-    imprimirBio() {
-        console.log(`${this.nombre}`);
-    }
-}
-const yo = new Persona("Jose", 39, "masculino", "casado");
-yo.imprimirBio();
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => text.length > 3;
+    Validations.validateDate = (myDate) => !isNaN(myDate.valueOf());
+})(Validations || (Validations = {}));
+console.log(Validations.validateText("pep"));
 //# sourceMappingURL=main.js.map
