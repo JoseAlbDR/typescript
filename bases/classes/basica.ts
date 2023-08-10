30cdd7bda8cef32c6944d56fbae25290bd2a397d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-10 19:03:29 +0200

    feat: ts class creation short hand

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -1,18 +1,22 @@
 (() => {
   class Avenger {
-    private name: string;
-    private team: string;
-    public realName?: string;
+    // private name: string;
+    // private team: string;
+    // public realName?: string;
     static avgAge: number = 35;
 
-    constructor(name: string, team: string, realName?: string) {
-      this.name = name;
-      this.team = team;
-      this.realName = realName;
+    constructor(
+      private name: string,
+      private team: string,
+      public realName?: string
+    ) {
+      // this.name = name;
+      // this.team = team;
+      // this.realName = realName;
     }
   }
 
-  const antman: Avenger = new Avenger("Antman", "Capitan");
+  const antman: Avenger = new Avenger("Antman", "Capitan", "Scott Lang");
 
   console.log(antman);
   console.log(Avenger.avgAge);
