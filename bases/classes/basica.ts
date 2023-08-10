dff85723021ea50d67c214985852b49d175efa4c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-10 19:08:34 +0200

    feat: public and private methods

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -4,6 +4,9 @@
     // private team: string;
     // public realName?: string;
     static avgAge: number = 35;
+    static getAvgAge(): number {
+      return this.avgAge;
+    }
 
     constructor(
       private name: string,
@@ -14,10 +17,19 @@
       // this.team = team;
       // this.realName = realName;
     }
+
+    public bio(): string {
+      return `${this.name} ${this.team}`;
+    }
+    // private bio(): string {
+    //   return `${this.name} ${this.team}`;
+    // }
   }
 
   const antman: Avenger = new Avenger("Antman", "Capitan", "Scott Lang");
 
   console.log(antman);
   console.log(Avenger.avgAge);
+  console.log(Avenger.getAvgAge());
+  console.log(antman.bio());
 })();
--- bases/main.js
+++ bases/main.js
@@ -1,15 +1,23 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.avgAge;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+        }
+        bio() {
+            return `${this.name} ${this.team}`;
         }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger("Antman", "Capitan", "Scott Lang");
     console.log(antman);
     console.log(Avenger.avgAge);
+    console.log(Avenger.getAvgAge());
+    console.log(antman.bio());
 })();
 //# sourceMappingURL=main.js.map
