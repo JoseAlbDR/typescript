4db96494989af80d1ac68a9df4ac7f9132d52e8a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-11 09:20:11 +0200

    feat: first decorator

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,14 @@
-import { getPokemon } from "./genercis/get-pokemon";
+// import { getPokemon } from "./genercis/get-pokemon";
 
-console.log(
-  getPokemon(1)
-    .then((pokemon) => console.log(pokemon.sprites.front_default))
-    .catch((err) => console.log(err))
-    .finally(() => console.log("Fin fetch"))
-);
+// console.log(
+//   getPokemon(1)
+//     .then((pokemon) => console.log(pokemon.sprites.front_default))
+//     .catch((err) => console.log(err))
+//     .finally(() => console.log("Fin fetch"))
+// );
+
+import { Pokemon } from "./decorators/pokemon";
+
+const charmander = new Pokemon("Charmander");
+
+console.log(charmander);
